7016a79c4e0d5e8491c1c17716361e8e35d476ac
billypurvis <purvisbilly@outlook.com>
2019-05-03 14:33:53 +0100

    Fix issue where  returns null and isn't a countable type resulting in a warning message

--- class-b2c-endpoint-handler.php
+++ class-b2c-endpoint-handler.php
@@ -12,7 +12,7 @@ class B2C_Endpoint_Handler {
 		$this->metadata_endpoint = B2C_Settings::metadata_endpoint_begin() . $policy_name;
 		$response = wp_remote_get($this->metadata_endpoint);
 		$decoded_response = json_decode($response['body'], true);
-		if (count($decoded_response) == 0 )
+		if (isset($decoded_response) || empty($decoded_response))
 			throw new Exception('Unable to retrieve metadata from ' . $this->metadata_endpoint);
 		
 		$this->metadata = $decoded_response;
